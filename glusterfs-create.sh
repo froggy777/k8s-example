8eb9120b703161ac950145ad565fdc3322c29c4e
froggy777 <phen1x07@mail.ru>
2018-12-06 21:19:42 +0000

    Add glusterFS

---
+++ glusterfs-create.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#VAR
+GLUSTER_DIR="/mnt/gluster"
+GLUSTER_NODE_1="ub"
+GLUSTER_NODE_2="ub2"
+GLUSTER_VOLUME="vg0"
+
+
+#Create dir
+mkdir $GLUSTER_DIR
+#Connect from node1 to node2
+gluster peer probe $GLUSTER_NODE_2
+echo "Don't forget to peer this node from another"
+#create volume on both nodes
+gluster volume create $GLUSTER_VOLUME replica 2 $GLUSTER_NODE_1:$GLUSTER_DIR $GLUSTER_NODE_2:$GLUSTER_DIR force
+#start volume
+gluster volume start $GLUSTER_VOLUME
+
+
+#Create endpoint and service in k8s
+kubectl create -f glusterfs/endpoints.json
+kubectl create -f glusterfs/service.json
+
+echo "
+To use  this volume add to pod/deployment:
+===========================
+spec:
+  containers:
+    volumeMounts:
+    - mountPath: "/path/to/mount/dir/in/container"
+      name: glusterfsvol
+  volumes:
+  - name: glusterfsvol
+    glusterfs:
+      endpoints: glusterfs-cluster
+      path: $GLUSTER_VOLUME
+      readOnly: false
+"
+
